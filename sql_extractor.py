c8dfdee66c4143566e7be628562c5a8cbb7d0cd3
rttf <74861784+RTF47@users.noreply.github.com>
2025-04-25 16:54:33 +0500

    Creating from scratch
    The working principle has been completely changed

--- sql_extractor.py
+++ sql_extractor.py
@@ -1,87 +1,227 @@
 import re
 
-
 def editing(llist: list[str]):
-    fake = []
+    edited = []
     for i in range(0, len(llist)):
-        stripped = llist[i].strip()
-        if stripped and stripped[0]!='#' and not any(j in 'Ѓ°ґ±ЊЂѓ' for j in llist[i]):
-            fake.append(llist[i].replace('\n',''))
-    return fake
+        stripped = llist[i]
+        if stripped and '#' not in stripped and not any(j in 'Ѓ°ґ±ЊЂѓ' for j in llist[i]):
+            edited.append(stripped)
+    return edited
 
 
-def sql_extractor():
-    with open('test.py') as f:
-        lines = editing(f.readlines())
-        possible_sql = []
-        sql = ''
-        stack = []
+def fill_parsed(lines: list):
+    parsed = {}
+    stack = []
+    value = ''
+    for line in lines:
+        linefake=line
+        if ('=' in linefake) and (not stack) and not any(ttype in linefake for ttype in ['.execute(','.executemany(','.fetch(','.read_sql(','.copy_expert(']):
+            linefake = re.sub(r'\s*=\s*', '=', linefake) #делает так, чтобы между var, = и value не было пробелов
+            equal = linefake.index('=')
+            var = linefake[:equal]
+            if '"""' in linefake:
+                qcount = linefake.count('"""')
+                if qcount == 2:
+                    woquote = linefake.replace('"""',"",1) 
+                    woquoteindex = woquote.index('"""')+6 
+                    value += linefake[equal+1:woquoteindex] 
+                    parsed[var]=value
+                    value = ''
+                else:
+                    stack = ['"""']
+                    if linefake[equal+1]!='(':
+                        value += linefake[equal+1:]
+                    else:
+                        value += linefake[equal+2:]
 
-        for line in lines:
+            elif "'''" in linefake:
+                qcount = linefake.count("'''")
+                if qcount == 2:
+                    woquote = linefake.replace("'''","",1) 
+                    woquoteindex = woquote.index("'''")+6 
+                    value += linefake[equal+1:woquoteindex] 
+                    parsed[var]=value
+                    value = ''
+                else:
+                    stack = ["'''"]
+                    if linefake[equal+1]!='(':
+                        value += linefake[equal+1:]
+                    else:
+                        value += linefake[equal+2:]
             
-            if 'execute(' in line or 'fetch(' in line or 'read_sql' in line:
-                start = 0
-                if 'execute(' in line:
-                    start = line.index('execute(')+8
+            elif '"' in linefake:
+                woquote = linefake.replace('"',"",1) 
+                woquoteindex = woquote.index('"')+2 
+                value += linefake[equal+1:woquoteindex] 
+                parsed[var]=value
+                value = ''
 
-                elif 'fetch(' in line:
-                    start = line.index('fetch(')+6
-                    
-                elif 'read_sql(' in line:
-                    start = line.index('read_sql(')+9
+            elif "'" in linefake:
+                woquote = linefake.replace("'","",1) 
+                woquoteindex = woquote.index("'")+2 
+                value += linefake[equal+1:woquoteindex] 
+                parsed[var]=value
+                value = ''
 
-                if (line[start] in "'" or line[start] in '"'):
-                        if not stack:
-                            stack = ['(']
+        else:
+            if stack:
+                if '"""' in stack:
+                    if '"""' in linefake:
+                        if linefake != '"""':
+                            stack = []
+                            woquoteindex = linefake.index('"""')+3
+                            value += linefake[:woquoteindex] 
+                            parsed[var]=value
+                            value = ''
+                        else:
+                            stack = []
+                            value += linefake
+                            parsed[var]=value
+                            value = ''
+                    else:
+                        value += linefake
+
+                elif "'''" in stack:
+                    if "'''" in linefake:
+                        if linefake != "'''":
+                            stack = [] 
+                            woquoteindex = linefake.index("'''")+3
+                            value += linefake[:woquoteindex] 
+                            parsed[var]=value
+                            value = ''
+                        else:
+                            stack = []
+                            value += linefake
+                            parsed[var]=value
+                            value = ''
+                    else:
+                        value += ' '+linefake
+
+    return parsed
+
+
+def extractor(lines):
+    parsed = fill_parsed(lines)
+    possible_sqls = []
+    stack = []
+    sql = ''
+    for ttype in ['.execute(','.executemany(','.fetch(','.read_sql(','.copy_expert(']:
+        for line in lines:
+            if ttype in line:
+                if '"""' in line and (line.index('(')+1==line.index('"""')):
+                    quotes = line.count('"""')
+                    if quotes == 2:
+                        firstquote = line.index('"""')
+                        woquote = line.replace('"""','',1)
+                        secondquote = woquote.index('"""')+6
                         
-                        for ch in range(start, len(line)):
-                            if not stack:
-                                break
-                                
-                            if line[ch] == '(':
-                                stack.append('(')
-                                sql += '('
-                            elif line[ch] == ')':
-                                stack.pop()
-                                if not stack:
-                                    sql = sql.replace('"','')
-                                    sql = sql.replace("'","")
-                                    possible_sql.append(sql)
-                                    sql = ''
-                                    break
-                            elif stack and line[ch] not in '()':
-                                sql += line[ch]
+                        sql = line[firstquote:secondquote]
+                        possible_sqls.append(sql)
+                        sql = ''
+                    else:
+                        stack = ['"""']
+                        firstquote = line.index('"""')
+                        sql = line[firstquote:]
 
-            elif stack:
-                        for ch in line:
-                            if ch == '(':
-                                stack.append('(')
-                                sql += '('
-                            elif ch == ')':
-                                stack.pop()
-                                if not stack:
-                                    sql = sql.replace('"','')
-                                    sql = sql.replace("'","")
 
-                                    possible_sql.append(sql[:len(sql)]+')')
-                                    sql = ''
-                                    break
-                            elif stack and ch not in '()':
-                                sql += ch
+                elif "'''" in line and (line.index('(')+1==line.index("'''")):
+                    quotes = line.count("'''")
+                    if quotes == 2:
+                        firstquote = line.index("'''")
+                        woquote = line.replace("'''",'',1)
+                        secondquote = woquote.index("'''")+6
+                        
+                        sql = line[firstquote:secondquote]
+                        possible_sqls.append(sql)
+                        sql = ''
+                    else:
+                        stack = ["'''"]
+                        firstquote = line.index("'''")
+                        sql = line[firstquote:]
 
-            elif any(word in line.upper() for word in ['SELECT','INSERT','UPDATE','DELETE','CREATE','ALTER','DROP','AUTO_INCREMENT','TEXT NOT NULL', "PRIMARY KEY"]):
-                        for ch in line:
-                            if ch == '(':
-                                stack.append('(')
-                                sql += '('
-                            elif ch == ')':
-                                stack.pop()
-                                sql += ')'
-                                if not stack:
-                                    possible_sql.append(sql[:len(sql)])
-                                    sql = ''
-                                    break
+                elif '"' in line and (line.index('(')+1==line.index('"')):
+                    firstquote = line.index('"')
+                    woquote = line.replace('"','',1)
+                    secondquote = woquote.index('"')+2
+                    sql = line[firstquote:secondquote]
+                    possible_sqls.append(sql)
+                    sql = ''
+
+                elif "'" in line and (line.index('(')+1==line.index("'")):
+                    firstquote = line.index("'")
+                    woquote = line.replace("'",'',1)
+                    secondquote = woquote.index("'")+2
+                    sql = line[firstquote:secondquote]
+                    possible_sqls.append(sql)
+                    sql = ''
+
+                elif not any(quote in line[line.index('(')+1] for quote in ['"""',"'''",'"',"'"]):
+                    if ',' not in line:
+                        skobka1 = line.index('(')+1
+                        skobka2 = line.index(')')
+                        var = line[skobka1: skobka2]
+                        sql = parsed[var]
+                        try:
+                            sql = parsed[var]
+                            possible_sqls.append(sql)
+                        except KeyError:
+                            print(f'Ключ "{var}" не найден.')
+                        sql = ''
+                    else:
+                        skobka1 = line.index('(')+1
+                        zapytaya = line.index(',')
+                        var = line[skobka1: zapytaya]    
+                        try:
+                            sql = parsed[var]
+                            possible_sqls.append(sql)
+                        except KeyError:
+                            print(f'Ключ "{var}" не найден.')
+                        sql = ''
+            else:
+                if stack:
+                    if '"""' in stack:
+                        if '"""' in line:
+                            if '"""' != line:
+                                stack = []
+                                quote = line.index('"""')+3
+                                sql += line[:quote]
+                                possible_sqls.append(sql)
+                                sql = ''
                             else:
-                                sql += ch                 
+                                stack = []
+                                sql += line
+                                possible_sqls.append(sql)
+                                sql = ''
+                        else:
+                            sql += line
 
-        return possible_sql                           
+                    elif "'''" in stack:
+                        if "'''" in line:
+                            if "'''" != line:
+                                stack = []
+                                quote = line.index("'''")+3
+                                sql += line[:quote]
+                                possible_sqls.append(sql)
+                                sql = ''
+                            else:
+                                stack = []
+                                sql += line
+                                possible_sqls.append(sql)
+                                sql = ''
+                        else:
+                            sql += line
+
+    return possible_sqls
+
+
+def main():
+    with open('test2.py') as f:
+        lines = editing(f.readlines())
+        possible_sqls = extractor(lines)
+        parsed = fill_parsed(lines)
+    return possible_sqls
+
+
+spisok = main()
+for i in spisok:
+    print(i)
